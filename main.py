e70edcedf8f34aa1edf2e1f3e71d27dbd7628666
TacticalCoders <docdoca@naver.com>
2022-09-23 21:48:18 +0900

    모델 불러오기

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
-import tokenization as tokenization
-def main(config):
+import transformers
+from transformers import AutoTokenizer
 
+
+def main():
     vocab_file = "./vocab_kisti.txt"
 
-    tokenizer = tokenization.FullTokenizer(
+    tokenizer = AutoTokenizer.FullTokenizer(
         vocab_file=vocab_file,
         do_lower_case=False,
-        tokenizer_type="Mecab"
-)
+        tokenizer_type="Mecab")
+
+    model_path = 'model/pytorch_model.bin'
+    config = transformers.BertConfig.from_pretrained('model/bert_config_kisti.json')
+    model = transformers.BertForPreTraining.from_pretrained(model_path, config=config)
+
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
@@ -17,5 +23,3 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
