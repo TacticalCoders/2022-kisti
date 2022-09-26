c3c4038cc5a0702ecbd0912e72b7fb2a36500aa7
TacticalCoders <docdoca@naver.com>
2022-09-27 00:58:55 +0900

    변경된 데이터셋에 맞게 파일 읽어오기 수정(fine_tag, coarse_tag 추가, is_key 삭제)

--- utils.py
+++ utils.py
@@ -4,9 +4,9 @@ import pandas as pd
 def read_data(fn):
     data = pd.read_json(fn)
 
-    labels = data.tag.to_list()
+    fine_tag = data.fine_tag.to_list()
+    coarse_tag = data.coarse_tag.to_list()
     texts = data.sentence.to_list()
     doc_id = data.doc_id.to_lits()
-    is_key = data.keysencence.to_list()
 
-    return labels, texts, doc_id, is_key
+    return fine_tag, coarse_tag, texts, doc_id
