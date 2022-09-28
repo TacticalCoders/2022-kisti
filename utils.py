fc2f262f06cad9d24acf8d5de6fd6a694c1f275f
TacticalCoders <docdoca@naver.com>
2022-09-28 20:51:11 +0900

    대분류 태그, 세부 분류 태그 정수 매핑 딕셔너리(index_to_tag) 반환

--- utils.py
+++ utils.py
@@ -4,9 +4,23 @@ import pandas as pd
 def read_data(fn):
     data = pd.read_json(fn)
 
-    fine_tag = data.fine_tag.to_list()
-    coarse_tag = data.coarse_tag.to_list()
+    fine_tags = data.fine_tag.to_list()
+    coarse_tags = data.coarse_tag.to_list()
     texts = data.sentence.to_list()
-    doc_id = data.doc_id.to_lits()
+    doc_ids = data.doc_id.to_lits()
 
-    return fine_tag, coarse_tag, texts, doc_id
+    return texts, fine_tags, coarse_tags, doc_ids
+
+
+def labels_to_map(fn):
+    """
+    :param fn: 태그(세부 태그, 대분류 태그)의 vocab 파일 (.txt)
+    :return: 정수_to_tag 딕셔너리
+    """
+    vocab_map = {}
+    with open(fn, 'r', encoding='utf-8') as vocab:
+        for label in vocab:
+            label = label.strip()
+            vocab_map[len(vocab_map)] = label
+
+    return vocab_map
